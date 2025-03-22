3e510c37d0d1af55aea6a282454e2b04fcfbce0d
Daniel Supernault <danielsupernault@gmail.com>
2025-03-21 18:24:04 -0600

    Add tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,24 @@
 module.exports = {
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+          primary: {
+            DEFAULT: '#3C82F6',
+            foreground: '#ffffff',
+          },
+          secondary: {
+            DEFAULT: '#e2e2e2',
+            foreground: '#111111',
+          },
+          accent: {
+            DEFAULT: '#f4f4f4',
+            foreground: '#111111',
+          },
+          background: '#ffffff',
+          foreground: '#111111',
+          input: '#d1d5db',
+      },
+    }
     },
     plugins: [
       require('@tailwindcss/aspect-ratio'),
